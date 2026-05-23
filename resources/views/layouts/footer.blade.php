2825aa993aabf0e2f8ac38ee6e92f3dd7aa922ef
muhammadhaddou <mohamedhaddou19@gmail.com>
2026-05-23 04:18:59 +0100

    Remove copyright symbol and all rights reserved from footer

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,5 +1,5 @@
 <footer class="bg-white shadow mt-auto border-t">
     <div class="max-w-7xl mx-auto py-4 px-4 sm:px-6 lg:px-8">
-        <p class="text-center text-sm text-gray-500">&copy; {{ date('Y') }} Haddou-Mohamed ClinicOffshoring. {{ __('All rights reserved.') }}</p>
+        <p class="text-center text-sm text-gray-500">{{ date('Y') }} Haddou-Mohamed ClinicOffshoring.</p>
     </div>
 </footer>
